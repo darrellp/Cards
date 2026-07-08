622f45b03ae25011107020c4103797f2f78d5602
Darrell Plank <jar_czar@msn.com>
2026-07-08 06:55:16 -0700

    Added Card images

---
+++ CardsTests/ImageTests.cs
@@ -0,0 +1,28 @@
+﻿using Cards;
+
+namespace CardsTests;
+
+public class ImageTests
+{
+    [Test]
+    public void TestStreams()
+    {
+        Card[] cards =
+        [
+            Card.CardFromString("2H"),
+            Card.CardFromString("KS")
+        ];
+        foreach (var card in cards)
+        {
+            try
+            {
+                using var stream = card.ImageStream();
+                Assert.That(stream, Is.Not.Null);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
