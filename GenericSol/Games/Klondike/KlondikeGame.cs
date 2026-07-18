20c87e62b928564cd04bd64e9754845c7645ce7a
Darrell Plank <jar_czar@msn.com>
2026-07-18 16:18:31 -0700

    First version of Generic Klondike game

---
+++ GenericSol/Games/Klondike/KlondikeGame.cs
@@ -0,0 +1,260 @@
+﻿using Cards;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace GenericSol.Games.Klondike;
+internal class KlondikeGame : GenericGame
+{
+    #region Constants
+    public const int TabCount = 7;
+    public const int FndCount = 4;
+    #endregion
+
+    #region Private members
+    // Whether we have detected any moves yet in the current run through the feed
+    private readonly Suit[] _fndSuits = [Suit.None, Suit.None, Suit.None, Suit.None];
+    #endregion
+
+    #region Stacks
+    // These are only internal for unit testing purposes
+    // ReSharper disable InconsistentNaming
+    internal MixedStack[] _tableau { get; } = new MixedStack[TabCount];
+    internal readonly Stack[] _foundations = new Stack[FndCount];
+    internal Stack _waste { get; private set; } = new Stack();
+    internal Stack _stock { get; private set; } = new Stack();
+    // ReSharper restore InconsistentNaming
+    #endregion
+
+
+
+    public override IList<IMove> GetMoves()
+    {
+        var moves = new List<GenericMove>();
+
+        // Check moves from the discard pile
+        if (_waste.Count != 0)
+        {
+            var discard = _waste.TopCard;
+
+            // Check for discard to foundation moves
+            var (fndIndex, fCanPlay) = CanPlayToFoundations(discard);
+            if (fCanPlay)
+            {
+                // new foundation stack
+                moves.Add(new GenericMove("Waste", FndNameFromIndex(fndIndex)));
+            }
+
+            // Discard to tableau moves
+            for (var iTab = 0; iTab < TabCount; iTab++)
+            {
+                // Check for moves from the discard pile to a tableau
+                if (discard.IsKBelow(_tableau[iTab].TopCard))
+                {
+                    moves.Add(new GenericMove("Waste", TabNameFromIndex(iTab)));
+                }
+            }
+        }
+
+        // Moves from the foundations
+        for (var iFnd = 0; iFnd < FndCount; iFnd++)
+        {
+            var fndCard = _foundations[iFnd].TopCard;
+            for (var iTabDst = 0; iTabDst < TabCount; iTabDst++)
+            {
+                if (fndCard.IsKBelow(_tableau[iTabDst].TopCard))
+                {
+                    moves.Add(new GenericMove(TabNameFromIndex(iFnd), FndNameFromIndex(iTabDst)));
+                }
+            }
+        }
+
+        // Moves from Tableau
+        for (var iTab = 0; iTab < TabCount; iTab++)
+        {
+            var tabCard = _tableau[iTab].TopCard;
+            var tabId = TabNameFromIndex(iTab);
+
+            // Tableau to foundation - here iTab tableau is source
+            var (fndIndex, fCanPlay) = CanPlayToFoundations(tabCard);
+            if (fCanPlay)
+            {
+                moves.Add(new GenericMove(TabNameFromIndex(iTab), FndNameFromIndex(fndIndex)));
+            }
+
+            // tableau to tableau
+            // Note: in this context iTab is the destination and we check over all other
+            // possible source tableaus
+            for (var iTabSrc = 0; iTabSrc < TabCount; iTabSrc++)
+            {
+                var (cCards, canPlay) = CanPlayTabToTab(iTabSrc, iTab);
+                if (canPlay)
+                {
+                    var tabIdSrc = TabNameFromIndex(iTabSrc);
+                    moves.Add(new GenericMove(tabIdSrc, tabId, cCards));
+                }
+            }
+        }
+
+        // Right now I'm not considering Foundation to Tableau to avoid circular logic problems.  I probably should
+        // include them and weed them out the same time I weed out circular dependencies in tableau to tableau moves.
+        // Resolving these circularities I think depends on always turning one more card up.  We need sometimes to
+        // think of one move as extending to two and the second always turns up a card or increases a foundation count.
+        // So if we can play a 3S from under a 4H onto another tableau, the only time we'll do it is if the 4H can be
+        // be played immediately thereafter onto the aces.  For the foundations to tableau we'll only do that if the
+        // next move can be another tableau onto the former foundation card which results in turning up a card.
+        // Really, this probably should be done a level above FindAllMoves which should echo it's name and find ALL
+        // moves, leaving the weeding out to a higher level. 
+        return (IList<IMove>)moves;
+    }
+
+    private (int cCards, bool canPLay) CanPlayTabToTab(int iSrc, int iDst)
+    {
+        var srcStack = _tableau[iSrc];
+        var dstStack = _tableau[iDst];
+        var noMove = (-1, false);
+
+        // Can't play from tableau stack to the same tableau stack or move empty stack
+        if (iSrc == iDst || srcStack.Count == 0)
+        {
+            return noMove;
+        }
+
+        // If the destination is empty we can only move a King
+        if (dstStack.Count == 0)
+        {
+            return srcStack.FirstFaceupCard.Rank == Card.KING ? (srcStack.CardsUp, true) : noMove;
+        }
+
+        // BOTH STACKS ARE OCCUPIED
+
+        var srcTop = srcStack.TopCard;
+        var dstTop = dstStack.TopCard;
+
+        // Can't place larger cards on smaller ones...
+        if (srcTop.Rank >= dstTop.Rank)
+        {
+            return noMove;
+        }
+
+        var rankDifference = dstTop.Rank - srcTop.Rank;
+
+        // We have to have enough cards in source pile to come up to proper rank for the dst pile
+        if (srcStack.CardsUp < rankDifference)
+        {
+            return noMove;
+        }
+
+        // Finally, the colors have to be opposite
+        if (srcStack[^rankDifference].IsSameColor(dstTop))
+        {
+            return noMove;
+        }
+
+        // If we've survived all the previous tests then it's a valid move
+        return (rankDifference, true);
+    }
+
+
+    internal (int iFnd, bool canPlay) CanPlayToFoundations(Card card)
+    {
+        if (card == Card.NullCard)
+        {
+            return (-1, false);
+        }
+
+        var fndIndex = FndIndexFromSuit(card.Suit);
+        var fndStack = _foundations[fndIndex];
+        if (fndStack.Count == 0 && card.Rank == Card.ACE || fndStack.TopCard.Rank == card.Rank - 1)
+        {
+            return (fndIndex, true);
+        }
+
+        return (-1, false);
+    }
+
+    /// <summary>
+    /// Find the index into _foundations of a suitable stack given a suit
+    /// </summary>
+    ///
+    /// <remarks>
+    /// If there already is a stack dedicated to this suit return it, otherwise return the first empty foundation
+    /// </remarks>
+    /// 
+    /// <param name="suit">The suit we're searching for</param>
+    /// 
+    /// <returns>Index into _tableau which gives a suitable foundation for this suit</returns>
+    private int FndIndexFromSuit(Suit suit)
+    {
+        var indexAssigned = -1;
+        var indexUnassigned = -1;
+        for (var i = 0; i < FndCount; i++)
+        {
+            if (_fndSuits[i] == suit)
+            {
+                indexAssigned = i;
+                break;
+            }
+            if (_fndSuits[i] == Suit.None && indexUnassigned < 0)
+            {
+                indexUnassigned = i;
+            }
+        }
+
+        Debug.Assert(indexAssigned >= 0 || indexUnassigned >= 0);
+        return indexAssigned >= 0 ? indexAssigned : indexUnassigned;
+    }
+
+    string FndNameFromIndex(int index)
+    {
+        if (index < 0 || index >= FndCount)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Foundation index must be between 0 and 3.");
+        }
+        return $"fnd{index}";
+    }
+
+    string TabNameFromIndex(int index)
+    {
+        if (index < 0 || index >= TabCount)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Tableau index must be between 0 and 6.");
+        }
+        return $"tab{index}";
+    }
+
+    public override Stack StackFromName(string name)
+    {
+        if (name.StartsWith("tab"))
+        {
+            var index = int.Parse(name.Substring(3));
+            if (index > 7)
+            {
+                throw new ArgumentOutOfRangeException(nameof(name), "Tableau index must be between 0 and 6.");
+            }
+            return _tableau[index];
+        }
+        else if (name.StartsWith("fnd"))
+        {
+            var index = int.Parse(name.Substring(3));
+            if (index > 3)
+            {
+                throw new ArgumentOutOfRangeException(nameof(name), "Foundation index must be between 0 and 3.");
+            }
+            return _foundations[index];
+        }
+        else if (name == "stock")
+        {
+            return _stock;
+        }
+        else if (name == "waste")
+        {
+            return _waste;
+        }
+        else
+        {
+            throw new ArgumentException($"Invalid stack name: {name}");
+        }
+    }
+}
